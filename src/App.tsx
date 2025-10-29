cdeda28aca5ab7aa1cdccb56abe8d01ca0b333e2
hritiksinghyadav <hritiksinghyadav02@gmail.com>
2025-10-29 22:05:51 +0530

    [HY_ZL_OO]: removed theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/Routes";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import { ThemeProvider } from "./context/ThemeContext";
 
 export const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider>
         <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
-      </ThemeProvider>
     </Provider>
   );
 };
--- src/components/TestComponent/Test.tsx
+++ src/components/TestComponent/Test.tsx
@@ -3,21 +3,16 @@ import React from "react";
 import { app } from "@/firebase/firebase";
 import Title from "antd/lib/typography/Title";
 import { Button, Card } from "antd";
-import { useTheme } from "@/context/ThemeContext";
 
 export const Test: React.FC = () => {
   console.log("firebase connected", app.name);
-  const { theme, toggleTheme } = useTheme();
 
   return (
     <>
       <Title level={1}>Konnichiwa - Firebase Connected!!</Title>
       <div style={{ padding: 24 }}>
-        <Card title="Theme Demo">
-          <p>Current theme: {theme}</p>
-          <Button type="primary" onClick={toggleTheme}>
-            Toggle Theme
-          </Button>
+        <Card title="Ant Design Demo">
+          <Button type="primary">Primary Action</Button>
         </Card>
       </div>
     </>
